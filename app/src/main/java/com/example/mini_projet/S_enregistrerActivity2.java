55da7833c3a63a4c3bfb8ecb8e5a114f2ea9c83d
Kaoutar Ahannuk <kaoutarahanuk@gmail.com>
2021-01-08 23:37:00 +0100

    Page4

---
+++ app/src/main/java/com/example/mini_projet/S_enregistrerActivity2.java
@@ -0,0 +1,14 @@
+package com.example.mini_projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class S_enregistrerActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s_enregistrer2);
+    }
+}
