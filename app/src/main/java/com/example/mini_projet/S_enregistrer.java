fcce2e8594e7f6841f48e4dab8cd37c16b3e9dd4
Fatima Bzioui <58828986+Fatima-Bzioui@users.noreply.github.com>
2021-01-10 19:02:53 +0100

    modif1

--- app/src/main/java/com/example/mini_projet/S_enregistrer.java
+++ app/src/main/java/com/example/mini_projet/S_enregistrer.java
@@ -3,9 +3,19 @@ package com.example.mini_projet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class S_enregistrer extends AppCompatActivity {
-
+    public static final String TAG = "TAG";
+    private EditText editTextNomComplet,editTextEmial,editTextPassword,editTextConfirmPassword;
+    private Button buttonSenregistrer;
+    private FirebaseAuth fAuth;
+    private FirebaseFirestore fStore;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
