4c11defcb6adb887ff7fbe30ac55aed09cad500d
Kaoutar Ahannuk <kaoutarahanuk@gmail.com>
2021-03-17 22:05:01 +0100

    modif

--- app/src/main/java/com/example/mini_projet/PageActivity.java
+++ app/src/main/java/com/example/mini_projet/PageActivity.java
@@ -2,13 +2,17 @@ package com.example.mini_projet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class PageActivity extends AppCompatActivity {
     EditText somme1, reste1, depense1;
-    Button voirPlus, ajout;
+    Button voirPlus, ajout,btn_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,18 @@ public class PageActivity extends AppCompatActivity {
         depense1=findViewById(R.id.somme1);
         voirPlus=findViewById(R.id.voirPlus);
         ajout=findViewById(R.id.ajout);
+        btn_out=findViewById(R.id.out);
+
         somme1.setText(getIntent().getStringExtra("somme"));
+
+
+        btn_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();//logout
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        });
     }
 }
