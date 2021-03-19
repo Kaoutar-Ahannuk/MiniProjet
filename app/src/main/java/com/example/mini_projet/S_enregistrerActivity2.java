fcbe6d16a6f309e94875c0b07a3dda5cbd1dddee
Fatima Bzioui <58828986+Fatima-Bzioui@users.noreply.github.com>
2021-03-19 14:40:58 +0100

    somme

--- app/src/main/java/com/example/mini_projet/S_enregistrerActivity2.java
+++ app/src/main/java/com/example/mini_projet/S_enregistrerActivity2.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -35,27 +36,19 @@ import java.util.HashMap;
 import java.util.Map;
 import com.example.mini_projet.S_enregistrer;
 
-public class S_enregistrerActivity2 extends AppCompatActivity implements ExampleDialog.ExampleDialogListener {
+public class S_enregistrerActivity2 extends AppCompatActivity {
 
     public static final String TAG = "TAG";
-    private EditText somme;
+    private TextView somme;
     private ImageButton next;
     private Button btn_out;
-    private ListView list1,list2;
     private FirebaseAuth fAuth;
     private FirebaseFirestore fStore;
     private String userID;
-    //variables select
-    TextView select,select2;
-    boolean[] selectedDepense,selectedRessource;
-    ArrayList<String> depenseList = new ArrayList<>();
-    ArrayList<String> ressourceList = new ArrayList<>();
-    String[] depenseArray = {"Maison","food","Medicament","Telephone","Eau et electricite","sport","argent de poche","Autres"};
-    String[] ressourceArray = {"Salaire","Bourse","Autres"};
-    ArrayAdapter adapter,adapter2;
-
-    private TextView txthome;
-    private TextView txtfod;
+    private CheckBox checkSalaire,checkBourse,checkAutres;
+    private EditText salaire,bourse,autres;
+    private TextView textViewSomme;
+    private Button buttonSomme;
 
 
 
@@ -71,154 +64,42 @@ public class S_enregistrerActivity2 extends AppCompatActivity implements Example
         btn_out = findViewById(R.id.out);
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
-        select = findViewById(R.id.select);
-        list1=findViewById(R.id.list1);
-        select2 = findViewById(R.id.select2);
-        list2 = findViewById(R.id.list2);
 
-        txthome = findViewById(R.id.txthome);
-        txtfod = findViewById(R.id.txtfod);
+        checkSalaire = findViewById(R.id.checkSalaire);
+        checkBourse = findViewById(R.id.checkBourse);
+        checkAutres = findViewById(R.id.checkAutres);
+        salaire = findViewById(R.id.salaire);
+        bourse = findViewById(R.id.bourse);
+        autres = findViewById(R.id.autres);
+        textViewSomme = findViewById(R.id.textViewSomme);
+        buttonSomme = findViewById(R.id.buttonSomme);
 
-        //initialize selected depense array and ressource array
-        selectedDepense = new boolean[depenseArray.length];
-        selectedRessource = new boolean[ressourceArray.length];
-
-        select.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openDialog();
-            }
-        });
-/*
-        select.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View view) {
-                //initialize alert dialog
-                AlertDialog.Builder builder = new AlertDialog.Builder(
-                        S_enregistrerActivity2.this
-                );
-
-                builder.setTitle("Selectionner vos depenses");
-                builder.setCancelable(false);
-                builder.setMultiChoiceItems(depenseArray, selectedDepense, new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
-                        if(b){
-                            depenseList.add(depenseArray[i]);
-                            Collections.sort(depenseList);
-                        }else {
-                            depenseList.remove(depenseArray[i]);
-                        }
-                    }
-                });
-
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @SuppressLint("ResourceType")
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        /*StringBuilder stringBuilder = new StringBuilder();
-                        for(int j=0; j<depenseList.size(); j++){
-                            stringBuilder.append(depenseArray[depenseList.get(j)]);
-                            if(j != depenseList.size()-1){
-                                stringBuilder.append(", ");
-                            }
-                        }
-
-                        select.setText(stringBuilder.toString());
-
-                        adapter =new ArrayAdapter(S_enregistrerActivity2.this,R.layout.element,R.id.t1,depenseList);
-                        list1.setAdapter(adapter);
-
-                        String[] items = new String[adapter.getCount()];
-                        for(int j= 0; j < adapter.getCount(); j++){
-                            items[j] = (String) adapter.getItem(j);
-                            Log.d("TAG", "Item nr: " +j+ " "+ adapter.getItem(j));
-                        }
-                    }
-                });
-
-                builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                });
-
-                builder.setNeutralButton("Supprimer tous", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        for(int j=0; j<selectedDepense.length; j++) {
-                            selectedDepense[j] = false;
-                            list1.removeViewAt(j);
-                            adapter.notifyDataSetChanged();
-                        }
-                    }
-                });
-
-                builder.show();
-            }
-        });
-        select2.setOnClickListener(new View.OnClickListener() {
+        buttonSomme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //initialize alert dialog
-                final AlertDialog.Builder builder = new AlertDialog.Builder(
-                        S_enregistrerActivity2.this
-                );
+                int sum=0;
+                if(checkSalaire.isChecked()){
+                    int sal = Integer.parseInt(salaire.getText().toString());
+                    sum += sal;
+                    textViewSomme.setText("somme");
+                    somme.setText(String.valueOf(sum));
+                }
+                if(checkBourse.isChecked()){
+                    int bours = Integer.parseInt(bourse.getText().toString());
+                    sum += bours;
+                    textViewSomme.setText("somme");
+                    somme.setText(String.valueOf(sum));
+                }
+                if(checkAutres.isChecked()){
+                    int autre = Integer.parseInt(salaire.getText().toString());
+                    sum += autre;
+                    textViewSomme.setText("somme");
+                    somme.setText(String.valueOf(sum));
+                }
 
-                builder.setTitle("Selectionner vos ressources");
-                builder.setCancelable(false);
-                builder.setMultiChoiceItems(ressourceArray, selectedRessource, new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
-                        if(b){
-                            ressourceList.add(ressourceArray[i]);
-                            Collections.sort(ressourceList);
-                        }else {
-                            ressourceList.remove(ressourceArray[i]);
-                        }
-                    }
-                });
-
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        /*StringBuilder stringBuilder = new StringBuilder();
-                        for(int j=0; j<depenseList.size(); j++){
-                            stringBuilder.append(depenseArray[depenseList.get(j)]);
-                            if(j != depenseList.size()-1){
-                                stringBuilder.append(", ");
-                            }
-                        }
-
-                        select.setText(stringBuilder.toString());
-
-                        adapter2 =new ArrayAdapter(S_enregistrerActivity2.this,R.layout.element,R.id.t1,ressourceList);
-                        list2.setAdapter(adapter2);
-                    }
-                });
-
-                builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                });
-
-                builder.setNeutralButton("Supprimer tous", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        for(int j=0; j<selectedRessource.length; j++) {
-                            selectedRessource[j] = false;
-                            list2.removeViewAt(j);
-                            adapter2.notifyDataSetChanged();
-                        }
-                    }
-                });
-
-                builder.show();
             }
-        });*/
+        });
+
 
 
         next.setOnClickListener(new View.OnClickListener() {
@@ -267,14 +148,4 @@ public class S_enregistrerActivity2 extends AppCompatActivity implements Example
         });
     }
 
-    public void openDialog() {
-        ExampleDialog exampleDialog = new ExampleDialog();
-        exampleDialog.show(getSupportFragmentManager(), "example dialog");
-    }
-
-    @Override
-    public void applyTexts(String username, String password) {
-        txthome.setText(username);
-        txtfod.setText(password);
-    }
 }
